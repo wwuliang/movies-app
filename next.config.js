65b2c14452188a4bb52d380c7ffb15cf52618b3a
wwuliang <williamwuliang22@gmail.com>
2023-12-29 12:44:02 -0500

    Set up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiKey: process.env.API_KEY, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiKey: process.env.API_KEY,
+  },
 }
-
-module.exports = nextConfig
---
+++ pages/about.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function About() {
+    return (
+        <div className="containter">
+            <Head>
+                <title>
+                    About Us
+                </title>
+                <link rel="stylesheet" href="/styles.css" />
+            </Head>
+            <h1>
+                About
+            </h1>
+        </div>
+    )
+}
