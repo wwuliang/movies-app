d8e6afa3f1b7de05ccbeedff2dd5396a73a0862b
wwuliang <williamwuliang22@gmail.com>
2023-12-29 13:30:14 -0500

    Search bar

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,25 @@ const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 export default function Home(initialData) {
   const [ searchResults, setSearchResults ] = useState([]);
+  const [ formInput, setFormInput ] = useState({});
+  const [ searchTerm, setSearchTerm ] = useState('');
 
   useEffect(() => {
     setSearchResults(initialData.trendingMovies.results);
   }, [initialData]);
+
+  const handleInput = (event) => {
+    let {name, value} = event.target;
+    setFormInput({...formInput, [name]: value});
+    setSearchTerm(event.target.value);
+  }
+
+  const search = async (event) => {
+    event.preventDefault();
+    let movies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&query=${formInput.searchTerm}&page-1&include_adult=false`);
+    movies = await movies.json();
+    setSearchResults(movies.results);
+  }
 
   return (
     <div className="containter">
@@ -19,10 +34,23 @@ export default function Home(initialData) {
           <link rel="icon" href="/favicon.ico"/>
           <link rel="stylesheet" href="/styles.css"/>
       </Head>
+      <div>
+          <form onSubmit={search}>
+            <input className="search" name="searchTerm" value={searchTerm} onChange={handleInput} type="text" required />
+            <button className="btn-search">
+              Search
+            </button>
+          </form>
+      </div>
       <div className="movie-search-results-grid">
         {searchResults.map((each, index) => {
           return (
-            <Movie index={each.id} title={each.title} poster_path={each.poster_path} overview={each.overview}/>
+            <Movie 
+              index={each.id} 
+              title={each.title} 
+              poster_path={each.poster_path} 
+              overview={each.overview}
+            />
           )
         })}
       </div>
