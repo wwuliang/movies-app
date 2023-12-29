4f6e49ce9ca53cb69933d2369635d11669700b6b
wwuliang <williamwuliang22@gmail.com>
2023-12-29 13:04:57 -0500

    Successfully implemented the movie data base into the index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head';
+import getConfig from 'next/config';
+import Movie from '@/src/components/Movie'
+import { useState, useEffect } from 'react';
 
-export default function Home() {
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+export default function Home(initialData) {
+  const [ searchResults, setSearchResults ] = useState([]);
+
+  useEffect(() => {
+    setSearchResults(initialData.trendingMovies.results);
+  }, [initialData]);
+
   return (
     <div className="containter">
       <Head>
@@ -8,6 +19,22 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico"/>
           <link rel="stylesheet" href="/styles.css"/>
       </Head>
+      <div className="movie-search-results-grid">
+        {searchResults.map((each, index) => {
+          return (
+            <Movie index={each.id} title={each.title} poster_path={each.poster_path} overview={each.overview}/>
+          )
+        })}
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  let trendingMovies = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${serverRuntimeConfig.apiKey}`);
+  trendingMovies = await trendingMovies.json();
+  console.log(trendingMovies);
+  return {
+    props: {trendingMovies: trendingMovies},
+  }
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+const Movie = ({ title, index, overview, poster_path }) => {
+    const IMAGE_API = 'https://image.tmdb.org/t/p/w500/';
+
+    return (
+        <div className="movie" key={index}>
+            <h3>
+                {title}
+            </h3>
+            <img src={IMAGE_API + poster_path} alt={title}/>
+            <div className="movie-overview">{overview}</div>
+        </div>
+    )
+}
+
+export default Movie;
